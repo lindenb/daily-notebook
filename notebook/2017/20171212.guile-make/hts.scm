3a83c992df6c6f4fa5e5c22ad7adb40e5541f2fd
lindenb <plindenbaum@yahoo.fr>
2017-12-12 17:32:35 +0100

    cont

--- notebook/2017/20171212.guile-make/hts.scm
+++ notebook/2017/20171212.guile-make/hts.scm
@@ -11,3 +11,8 @@
 (define (variant-file name)
 	"OOKKKKKKKKKKKKKK" 
 	)
+	
+;;  (filter (lambda (s) (> (string-length s) 0)) (string-split "a  bb      c   d e " #\space ) )
+;; (string-split "a  bb      c   d e " (lambda (c) (char-whitespace? c)))
+;; (filter (lambda (s) (> (string-length s) 0)) (string-split "a  bb      c   d\te\t\te" (lambda (c) (char-whitespace? c))) )
+
