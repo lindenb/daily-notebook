3469a38d2295acf7989890a6695c8e5808584921
Pierre Lindenbaum <plindenbaum@yahoo.fr>
2017-12-12 20:45:34 +0100

    cont

--- notebook/2017/20171212.guile-make/hts.scm
+++ notebook/2017/20171212.guile-make/hts.scm
@@ -11,6 +11,15 @@
 (define (variant-file name)
 	"OOKKKKKKKKKKKKKK" 
 	)
+
+
+;; Loading Readline Support
+
+;; (use-modules (ice-9 readline))
+;; (activate-readline)
+
+;; ((lambda (w s) (and (> (string-length w) (string-length s)) (string=? (substring w (- (string-length w) (string-length s)) ) s) )) "a.txt" ".txt") 
+
 	
 ;;  (filter (lambda (s) (> (string-length s) 0)) (string-split "a  bb      c   d e " #\space ) )
 ;; (string-split "a  bb      c   d e " (lambda (c) (char-whitespace? c)))
