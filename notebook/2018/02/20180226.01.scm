1f99a964e179fff9c482866ae76fb3c45e22fa1d
lindenb <plindenbaum@yahoo.fr>
2018-02-27 15:50:43 +0100

    cont

--- notebook/2018/02/20180226.01.scm
+++ notebook/2018/02/20180226.01.scm
@@ -1,16 +1,27 @@
 (define (runbatch w)
-(let* ((imagesize w)
+(let* (
+      (imagesize w)
       (img (car(gimp-image-new imagesize imagesize RGB)))
       (bg (car (gimp-layer-new img imagesize imagesize RGBA-IMAGE "bg" 100 NORMAL-MODE)))
-)
+      (y 0)
+      )
 
 
 (gimp-image-add-layer img bg 0)
 (gimp-drawable-set-visible bg TRUE)
 (gimp-drawable-fill bg 2)
+;; https://github.com/dchest/tinyscheme/blob/master/init.scm
+(set! y 0)
+(while (< y imagesize)
+	(gimp-rect-select img 0 y imagesize (+ 1 (random 5)) REPLACE 0 0)
+	(gimp-edit-fill bg FOREGROUND-FILL )
+	(set! y (+ y 10))
+)
+
+
 (gimp-image-flatten img)
 (gimp-xcf-save 1 img img "jeter.xcf"  "jeter.xcf")
-
+(display "done#################################\n")
 
 )
 ;(gimp-quit 0)
--- notebook/2018/02/20180226.gimp.batch.mk
+++ notebook/2018/02/20180226.gimp.batch.mk
@@ -1,12 +1,17 @@
 GIMP=gimp -i  -b -
 
-SCM=01
+SCM=01 02
 
-%.flag : %.scm
+%.flag : %.scm lenna.png
 	cat $< | $(GIMP)
 	touch $@
 
 all: $(addsuffix .flag,$(addprefix 20180226.,$(SCM)))
 
 
+lenna.png:
+	wget -O $@ "https://upload.wikimedia.org/wikipedia/en/2/24/Lenna.png"
+	
+clean:
+	rm -f lenna.png jeter.xcf
 
