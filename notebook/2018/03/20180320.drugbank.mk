294c40de3225f4a17f045fde7d53844a13b1725c
lindenb <plindenbaum@yahoo.fr>
2018-03-21 17:37:21 +0100

    cont

--- notebook/2018/03/20180320.drugbank.mk
+++ notebook/2018/03/20180320.drugbank.mk
@@ -1,7 +1,14 @@
-SHELL=/bin/bash
-all: Drugbank.java
-	rm -rf tmp
-	mkdir -p tmp
-	xjc -d tmp "https://www.drugbank.ca/docs/drugbank.xsd"
+DATABASE?=${HOME}/full_database.xml
+all: tmp/Drugbank.class $(DATABASE)
+	echo "Scanning the XML drug-bank file $(DATABASE)"
+	cat $(DATABASE) | java -cp tmp Drugbank
+
+tmp/Drugbank.class: Drugbank.java tmp/ca/drugbank/ObjectFactory.java
 	javac -d tmp -cp tmp Drugbank.java `find tmp/ca/ -type f -name '*.java'`
-	cat ${HOME}/database.xml | java -cp tmp Drugbank
+	
+
+tmp/ca/drugbank/ObjectFactory.java:
+	mkdir -p tmp && xjc -d tmp "https://www.drugbank.ca/docs/drugbank.xsd" && touch -c $@
+
+clean:
+	rm -rf tmp
--- notebook/2018/03/Drugbank.java
+++ notebook/2018/03/Drugbank.java
@@ -1,35 +1,73 @@
+/*
+Author: Pierre Lindenbaum @yokofakun
+
+related: 
+http://bionics.it/posts/parsing-drugbank-xml-or-any-large-xml-file-in-streaming-mode-in-go
+
+*/
 import ca.drugbank.*;
 import java.util.*;
+import java.util.stream.*;
+import java.io.*;
 import javax.xml.stream.*;
 import javax.xml.stream.events.*;
 import javax.xml.bind.*;
 import javax.xml.*;
+
 public class Drugbank
 {
 public static void main(final String args[]) throws Exception {
-	JAXBContext jc = JAXBContext.newInstance("ca.drugbank");
-	Unmarshaller	unmarshaller =jc.createUnmarshaller();
+	final JAXBContext jc = JAXBContext.newInstance("ca.drugbank");
+	final Unmarshaller	unmarshaller =jc.createUnmarshaller();
 	final XMLInputFactory xmlInputFactory=XMLInputFactory.newFactory();
 	xmlInputFactory.setProperty(XMLInputFactory.IS_NAMESPACE_AWARE, Boolean.TRUE);
-  XMLEventReader r=xmlInputFactory.createXMLEventReader(System.in);		
+	final XMLEventReader r=xmlInputFactory.createXMLEventReader(System.in);
+	final ExternalIdentifierResourceType EXTIDS[]={
+	  	ExternalIdentifierResourceType.CH_EMBL,
+	  	ExternalIdentifierResourceType.PUB_CHEM_COMPOUND,
+	  	ExternalIdentifierResourceType.PUB_CHEM_SUBSTANCE
+	  	};
+	final PrintStream w = System.out;	
 	while(r.hasNext())
-				{
-				final XMLEvent evt=r.peek();
-				if(evt.isStartElement() && evt.asStartElement().getName().getLocalPart().equals("drug") ) {
-				  final DrugType d = unmarshaller.unmarshal(r,DrugType.class).getValue();
-		  	  
-		  	 
-				  System.out.println(d.getName());
-				  System.out.println(d.getDescription());
-				  System.out.println(d.getCasNumber());
-				  System.out.println(d.getUnii());
-				  System.out.println("//");
-					
-					}
-				else {
-					r.next();//consumme
-					}
-				}
- r.close();
- }
+		{
+		final XMLEvent evt=r.peek();
+		if(evt.isStartElement() && evt.asStartElement().getName().getLocalPart().equals("drug") ) {
+		  final DrugType d = unmarshaller.unmarshal(r,DrugType.class).getValue();
+		  w.print(d.getName());
+		  w.print('\t');
+		  if(d.getCalculatedProperties()!=null) 
+		  	w.print(d.getCalculatedProperties().
+		  		getProperty().
+		  		stream().
+		  		filter(P->P.getKind().equals(CalculatedPropertyKindType.IN_CH_I_KEY)).
+		  		map(P->P.getValue()).
+		  		collect(Collectors.joining(" ")));
+		  	
+		  w.print('\t');
+		  if(d.getGroups()!=null)
+		  	w.print(d.getGroups().
+		  		getGroup().
+		  		stream().
+		  		map(G->G.value()).
+		  		collect(Collectors.joining("->")));
+		 
+	  	  for(final ExternalIdentifierResourceType extId: EXTIDS) {
+	  		w.print('\t');
+	  		if(d.getExternalIdentifiers()!=null)
+			  	w.print(d.getExternalIdentifiers().
+			  		getExternalIdentifier().
+			  		stream().
+			  		filter(P->P.getResource()==extId).
+			  		map(P->P.getIdentifier()).
+			  		collect(Collectors.joining(" ")));
+	  		}
+		  	
+		 w.println();
+		 }
+		else {
+		     r.next();//consumme
+		    }
+		}
+	r.close();
+	}
 }
